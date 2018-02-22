aa3992c33a7b20e5587896bb8565731de2eba69f
silva <htsilvakt04@gmail.com>
2018-02-22 14:21:15 +0700

    validate component with propType

--- app/index.js
+++ app/index.js
@@ -3,12 +3,47 @@ var ReactDOM = require('react-dom');
 
 require('./index.css');
 
-class App extends React.Component {
-    render () {
+class Avatar extends React.Component {
+    render() {
         return (
-            <h1>Hello Silva && ahihi</h1>
+            <img src={this.props.img} />
         )
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+class Label extends React.Component {
+    render() {
+        return (
+            <h1>Name: {this.props.name}</h1>
+        )
+    }
+}
+
+class ScreenName extends React.Component {
+    render() {
+        return (
+            <h3>Username: {this.props.username}</h3>
+        )
+    }
+}
+
+class Badge extends React.Component {
+    render() {
+        return (
+            <div>
+                <Avatar img={this.props.user.img}/>
+                <Label name={this.props.user.name}/>
+                <ScreenName username={this.props.user.username}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Badge user={{
+        name: 'Silva',
+        img: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460',
+        username: 'htsilvakt04'
+    }} />,
+    document.getElementById('root')
+);
